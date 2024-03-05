537e9f10755d10b87405edaca344626b51c6dec9
JimmyTran <quantrannhu@gmail.com>
2024-03-05 09:43:48 -0800

    Create Spring Boot Project and add JPA dependencies

---
+++ EnchantedJobPostings/src/main/java/com/skilldistillery/enchantedrealm/EnchantedJobPostingsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.enchantedrealm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnchantedJobPostingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnchantedJobPostingsApplication.class, args);
+	}
+
+}
---
+++ EnchantedJobPostings/src/test/java/com/skilldistillery/enchantedrealm/EnchantedJobPostingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.enchantedrealm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnchantedJobPostingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
