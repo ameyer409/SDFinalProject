e50327e1b0de3772f7c3847df194e34cad5ecf65
Allan Meyer <ameyer409@yahoo.com>
2024-03-12 11:13:34 -0400

    jobposting service created

--- ngEnchantedRealm/src/app/components/job-detail-applicant-view/job-detail-applicant-view.component.ts
+++ ngEnchantedRealm/src/app/components/job-detail-applicant-view/job-detail-applicant-view.component.ts
@@ -1,4 +1,8 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { JobpostingService } from '../../services/jobposting.service';
+import { Jobposting } from '../../models/jobposting';
 
 @Component({
   selector: 'app-job-detail-applicant-view',
@@ -8,8 +12,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './job-detail-applicant-view.component.css'
 })
 export class JobDetailApplicantViewComponent implements OnInit{
+
+  job: Jobposting | null = null;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private jobService: JobpostingService
+  ){}
+
   ngOnInit(): void {
     console.log('landing on details page');
+    this.activatedRoute.paramMap.subscribe({
+      next: (params) => {
+        let jobIdStr = params.get('id');
+        if (jobIdStr != null) {
+          let jobId = parseInt(jobIdStr);
+          if (!isNaN(jobId)) {
+            this.jobService.show(jobId).subscribe({
+              next: (jobPost) => {
+                  this.job = jobPost;
+              },
+              error: (err) => {
+                console.error('JobDetailAplicantViewComponent.ngOnInt(): error retreiving JobPosting:');
+                console.error(err);
+                this.router.navigateByUrl('notfound');
+              },
+            });
+          }
+          else{
+            this.router.navigateByUrl('notfound');
+          }
+        }
+      }
+    })
   }
 
 }
